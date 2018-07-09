d1541edb792f6b1ffd26a847b5674dc3fb473cd8
LightCMS <38491958+LightCMS@users.noreply.github.com>
2018-07-09 13:24:39 +0300

    Add files via upload

---
+++ design/js/script.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function(){
+  $(".slider").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000
+  });
+
+  $('.popup-btn').on('click', function(event){
+    event.preventDefault();
+    $('.popup').fadeIn();
+  });
+  $('.popup-close').on('click', function(event){
+    event.preventDefault();
+    $('.popup').fadeOut();
+  })
+
+  
+});
