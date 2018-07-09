af95ee061991d3a252216be8b04942144a7378d0
LightCMS <38491958+LightCMS@users.noreply.github.com>
2018-07-09 13:20:01 +0300

    Add files via upload

---
+++ outcode/js/script.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+  $(".slider").owlCarousel({
+  	items: 1,
+  	loop: true,
+    nav: true,
+  	autoplay: true,
+  	autoplayTimeout: 10000
+  });
+
+  $(".btn-nav").on("click", function() {
+  	var target = $(this).data("target");
+  	$(target).toggleClass("nav__list--open");
+  });
+
+  
+});
